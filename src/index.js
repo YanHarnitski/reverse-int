d432567cf5d33cc6ccbdb663e27847baec0805ab
YanHarnitski <yanharnitski@gmail.com>
2023-08-28 11:25:21 +0200

    feat: task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    n = Math.abs(n);
+    let toString = String(n);
+    let reverseString = toString.split("").reverse().join("");
+    let result = Number(reverseString);
+    return result;
+};
